afeb453cd215de77801f3330c014900ff7ebbc99
Shukorates <shubhamkumar.cse21@jecrc.ac.in>
2021-01-11 13:07:30 +0530

    added bjk

--- src/Sum.java
+++ src/Sum.java
@@ -1,9 +1,9 @@
 
 public class Sum {
-	
-	public static void main(String[] args)
-	{
-		System.out.print(2+2);
+
+	public static void main(String[] args) {
+		System.out.print(2 + 2);
+		System.out.println(8+3);
 	}
 
 }
